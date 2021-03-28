cd4deb6a3f21fcc2d2e7aa74e9785fc34b78ce7a
Wesley Chang <weschang15@gmail.com>
2021-03-28 14:22:03 -0700

    feat: use tailwind css for layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,16 @@
 import { graphql, useStaticQuery } from "gatsby"
 import PropTypes from "prop-types"
 import * as React from "react"
-import Header from "./header"
+import tw from "twin.macro"
+import Topbar from "./Topbar"
+
+const Container = tw.div`
+  w-full max-w-screen-sm	mx-auto
+`
+
+const Footer = tw.footer`
+  bg-gray-50 border-t border-gray-200 py-12 md:py-14 px-4 sm:px-6 lg:px-16 m-auto text-sm text-center
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,25 +32,16 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Topbar siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Container>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      </Container>
+      <Footer>
+        <p>
+          © {new Date().getFullYear()} Built by{" "}
+          <a href="https://www.wesleychang.dev">Wes Chang</a>
+        </p>
+      </Footer>
     </>
   )
 }
