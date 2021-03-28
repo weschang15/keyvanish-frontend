ebac1fa40aee63cf42e27a5e117564d65a170c14
Wesley Chang <weschang15@gmail.com>
2021-03-28 14:19:56 -0700

    feat: improve metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Keyvanish`,
+    description: `A better, more secret way to share secrets`,
+    author: `@codewithwes`,
+  },
+  proxy: {
+    prefix: `/api`,
+    url: `http://localhost:7777`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
