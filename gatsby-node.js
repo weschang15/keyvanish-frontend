55f34554305ebf3f4073eb49f19dedf118a3ce07
Wesley Chang <weschang15@gmail.com>
2021-03-31 14:20:51 -0700

    feat: create client-only routes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+  // Only update the `/app` page.
+  if (page.path.match(/^\/secrets/)) {
+    // page.matchPath is a special key that's used for matching pages
+    // with corresponding routes only on the client.
+    page.matchPath = "/secrets/*"
+    // Update the page.
+    createPage(page)
+  }
+}
